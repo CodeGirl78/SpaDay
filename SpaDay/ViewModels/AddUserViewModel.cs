cac597dbc5e5f38bbd2c2c9e6b9fbbd3dbf8ee9f
Melinda <codegirl78@gmail.com>
2021-09-20 11:12:10 -0500

    Added validation attributes

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required, StringLength(15, MinimumLength = 5, ErrorMessage = "Username between 5 and 15 characters.")]
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        [Required, StringLength(20, MinimumLength = 6, ErrorMessage = "Password bewteen 6 and 20 characters.")]
+        public string Password { get; set; }
+
+        [Required, StringLength(20, MinimumLength = 6, ErrorMessage = "Password bewteen 6 and 20 characters.")]
+        [Compare("Password", ErrorMessage = "Passwords must match.")]
+        public string VerifyPassword { get; set; }
+    }
+}
