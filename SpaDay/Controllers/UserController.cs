aa3e755921f27c527c5bc7f5f3950f2b9096b5b5
Melinda <codegirl78@gmail.com>
2021-09-21 17:49:13 -0500

    Code Clean Up Studio with Bonus Complete

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SpaDay.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SpaDay.Controllers
 {
